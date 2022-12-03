b427bfd1f2fc0cbbdc9dd4a009317b7f98c621c2
Trevor <tkhowell95@gmail.com>
2022-12-03 09:50:10 -0700

    Ran the husky hook on all relevant files

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,41 +1,41 @@
 import { ipcRenderer, contextBridge } from 'electron';
 
 declare global {
-  interface Window {
-    Main: typeof api;
-    ipcRenderer: typeof ipcRenderer;
-  }
+	interface Window {
+		Main: typeof api;
+		ipcRenderer: typeof ipcRenderer;
+	}
 }
 
 const api = {
-  /**
-   * Here you can expose functions to the renderer process
-   * so they can interact with the main (electron) side
-   * without security problems.
-   *
-   * The function below can accessed using `window.Main.sayHello`
-   */
-  sendMessage: (message: string) => {
-    ipcRenderer.send('message', message);
-  },
-  /**
+	/**
+	 * Here you can expose functions to the renderer process
+	 * so they can interact with the main (electron) side
+	 * without security problems.
+	 *
+	 * The function below can accessed using `window.Main.sayHello`
+	 */
+	sendMessage: (message: string) => {
+		ipcRenderer.send('message', message);
+	},
+	/**
     Here function for AppBar
    */
-  Minimize: () => {
-    ipcRenderer.send('minimize');
-  },
-  Maximize: () => {
-    ipcRenderer.send('maximize');
-  },
-  Close: () => {
-    ipcRenderer.send('close');
-  },
-  /**
-   * Provide an easier way to listen to events
-   */
-  on: (channel: string, callback: (data: any) => void) => {
-    ipcRenderer.on(channel, (_, data) => callback(data));
-  }
+	Minimize: () => {
+		ipcRenderer.send('minimize');
+	},
+	Maximize: () => {
+		ipcRenderer.send('maximize');
+	},
+	Close: () => {
+		ipcRenderer.send('close');
+	},
+	/**
+	 * Provide an easier way to listen to events
+	 */
+	on: (channel: string, callback: (data: any) => void) => {
+		ipcRenderer.on(channel, (_, data) => callback(data));
+	},
 };
 contextBridge.exposeInMainWorld('Main', api);
 /**
--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -3,47 +3,53 @@ import React, { useState } from 'react';
 import Icon from './assets/icons/Icon-Electron.png';
 
 function AppBar() {
-  const [isMaximize, setMaximize] = useState(false);
+	const [isMaximize, setMaximize] = useState(false);
 
-  const handleToggle = () => {
-    if (isMaximize) {
-      setMaximize(false);
-    } else {
-      setMaximize(true);
-    }
-    window.Main.Maximize();
-  };
+	const handleToggle = () => {
+		if (isMaximize) {
+			setMaximize(false);
+		} else {
+			setMaximize(true);
+		}
+		window.Main.Maximize();
+	};
 
-  return (
-    <>
-      <div className="py-0.5 flex justify-between draggable">
-        <div className="inline-flex">
-          <img className="h-6 lg:-ml-2" src={Icon} alt="Icon of Electron" />
-          <p className="text-xs md:pt-1 md:-ml-1 lg:-ml-2">Vite App</p>
-        </div>
-        <div className="inline-flex -mt-1">
-          <button onClick={window.Main.Minimize} className="undraggable md:px-4 lg:px-3 pt-1 hover:bg-gray-300">
-            &#8211;
-          </button>
-          <button onClick={handleToggle} className="undraggable px-6 lg:px-5 pt-1 hover:bg-gray-300">
-            {isMaximize ? '\u2752' : '⃞'}
-          </button>
-          <button onClick={window.Main.Close} className="undraggable px-4 pt-1 hover:bg-red-500 hover:text-white">
-            &#10005;
-          </button>
-        </div>
-      </div>
-      <div className="bg-gray-900 text-white undraggable">
-        <div className="flex text-center">
-          <div className="text-sm w-8  hover:bg-gray-700">File</div>
-          <div className="text-sm w-8   hover:bg-gray-700">Edit</div>
-          <div className="text-sm w-10  hover:bg-gray-700">View</div>
-          <div className="text-sm w-14  hover:bg-gray-700 ">Window</div>
-          <div className="text-sm w-9  hover:bg-gray-700 ">Help</div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="draggable flex justify-between py-0.5">
+				<div className="inline-flex">
+					<img className="h-6 lg:-ml-2" src={Icon} alt="Icon of Electron" />
+					<p className="text-xs md:-ml-1 md:pt-1 lg:-ml-2">Vite App</p>
+				</div>
+				<div className="-mt-1 inline-flex">
+					<button
+						onClick={window.Main.Minimize}
+						className="undraggable pt-1 hover:bg-gray-300 md:px-4 lg:px-3"
+					>
+						&#8211;
+					</button>
+					<button onClick={handleToggle} className="undraggable px-6 pt-1 hover:bg-gray-300 lg:px-5">
+						{isMaximize ? '\u2752' : '⃞'}
+					</button>
+					<button
+						onClick={window.Main.Close}
+						className="undraggable px-4 pt-1 hover:bg-red-500 hover:text-white"
+					>
+						&#10005;
+					</button>
+				</div>
+			</div>
+			<div className="undraggable bg-gray-900 text-white">
+				<div className="flex text-center">
+					<div className="w-8 text-sm  hover:bg-gray-700">File</div>
+					<div className="w-8 text-sm   hover:bg-gray-700">Edit</div>
+					<div className="w-10 text-sm  hover:bg-gray-700">View</div>
+					<div className="w-14 text-sm  hover:bg-gray-700 ">Window</div>
+					<div className="w-9 text-sm  hover:bg-gray-700 ">Help</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default AppBar;
