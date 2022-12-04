98864c3c07feba7fc01a825abbaa53134762ef1c
stevenshow <steven.schoebinger@gmail.com>
2022-12-04 14:12:36 -0700

    formatting header and menu

--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -16,19 +16,19 @@ function AppBar() {
 
 	return (
 		<>
-			<div className="draggable flex justify-between py-0.5">
+			<div className="draggable flex justify-between bg-gray-800 py-0.5">
 				<div className="inline-flex">
 					<img className="h-6 lg:-ml-2" src={Icon} alt="Icon of Electron" />
-					<p className="text-xs md:-ml-1 md:pt-1 lg:-ml-2">Vite App</p>
+					<p className="text-md text-gray-500">NotePad--</p>
 				</div>
-				<div className="-mt-1 inline-flex">
+				<div className="-my-1 inline-flex">
 					<button
 						onClick={window.Main.Minimize}
-						className="undraggable pt-1 hover:bg-gray-300 md:px-4 lg:px-3"
+						className="undraggable pt-1 hover:bg-gray-500 md:px-4 lg:px-3"
 					>
 						&#8211;
 					</button>
-					<button onClick={handleToggle} className="undraggable px-6 pt-1 hover:bg-gray-300 lg:px-5">
+					<button onClick={handleToggle} className="undraggable px-6 pt-1 hover:bg-gray-500 lg:px-5">
 						{isMaximize ? '\u2752' : '⃞'}
 					</button>
 					<button
@@ -39,13 +39,24 @@ function AppBar() {
 					</button>
 				</div>
 			</div>
-			<div className="undraggable bg-gray-900 text-white">
+			<div className="undraggable bg-gray-800 text-white">
 				<div className="flex text-center">
-					<div className="w-8 text-sm  hover:bg-gray-700">File</div>
-					<div className="w-8 text-sm   hover:bg-gray-700">Edit</div>
-					<div className="w-10 text-sm  hover:bg-gray-700">View</div>
-					<div className="w-14 text-sm  hover:bg-gray-700 ">Window</div>
-					<div className="w-9 text-sm  hover:bg-gray-700 ">Help</div>
+					<button
+						className="text-md w-8 hover:bg-gray-700"
+						onClick={() => {
+							console.log('File');
+						}}
+					>
+						File
+					</button>
+					<button
+						className="text-md w-8 hover:bg-gray-700"
+						onClick={() => {
+							console.log('Edit');
+						}}
+					>
+						Edit
+					</button>
 				</div>
 			</div>
 		</>
