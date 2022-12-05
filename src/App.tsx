10c9ebd1dcb49a48d8de648a4b9dd6b231461d6b
stevenshow <steven.schoebinger@gmail.com>
2022-12-04 20:47:57 -0700

    remove console.log()

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react';
 import AppBar from './AppBar';
 
 function App() {
-	console.log(window.ipcRenderer);
-
 	const [isOpen, setOpen] = useState(false);
 	const [isSent, setSent] = useState(false);
 	const [fromMain, setFromMain] = useState<string | null>(null);
